da9b629d2d9f637ca294fbf2ce2b95abc7a80a6f
Siva Donkada <sivadonkada2121@gmail.com>
2017-07-18 18:43:55 +0530

    Delete pyscreenshotimg.py

--- pyscreenshotimg.py
+++
@@ -1,9 +0,0 @@
-#-- include('examples/showgrabfullscreen.py') --#
-import pyscreenshot as ImageGrab
-
-
-if __name__ == "__main__":
-    # fullscreen
-    im=ImageGrab.grab()
-    im.save("/home/edx/1.jpg")
-#-#
